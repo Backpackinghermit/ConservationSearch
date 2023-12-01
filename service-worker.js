15a960e488a2df8556bf167f0dae76a2b5ea1fad
Backpackinghermit <121320002+Backpackinghermit@users.noreply.github.com>
2023-12-01 14:03:54 +0000

    Main site

---
+++ service-worker.js
@@ -0,0 +1,25 @@
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open('my-pwa-cache').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/style.css',
+        '/script.js',
+        '/search.html',
+        '/search-results.html',
+        'search.css',
+        'search-results.css',
+        
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
