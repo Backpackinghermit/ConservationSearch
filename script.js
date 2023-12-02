8c8e737ba729a096558a41bd88e397af0d6381b1
Backpackinghermit <121320002+Backpackinghermit@users.noreply.github.com>
2023-12-02 10:45:17 +0000

    PWA attempt

--- script.js
+++ script.js
@@ -1,28 +1,19 @@
-let deferredPrompt;
+let defferedPrompt;
+const addbtn = document.querySelector('.btn');
 
-window.addEventListener('beforeinstallprompt', (e) => {
-  // Prevent the mini-infobar from appearing on mobile
-  e.preventDefault();
-  // Save the event for later use
-  deferredPrompt = e;
-  // Update UI to show install button
-  btnInstall.style.display = 'block';
+window.addEventListener('beforeinstallprompt', event => {
+    event.preventDefault();
+    defferedPrompt = event
+    addbtn.style.display = 'block';
 });
 
-btnInstall.addEventListener('click', (e) => {
-  // Show the install prompt
-  deferredPrompt.prompt();
-  // Wait for the user to respond to the prompt
-  deferredPrompt.userChoice.then((choiceResult) => {
-    if (choiceResult.outcome === 'accepted') {
-      console.log('User accepted the install prompt');
-    } else {
-      console.log('User dismissed the install prompt');
-    }
-    deferredPrompt = null;
-  });
-});
+addbtn.addEventListener('click', event => {
+    defferedPrompt.prompt();
 
-window.addEventListener('appinstalled', (evt) => {
-  // App is installed, update your UI accordingly
-});
+    defferedPrompt.userChoice.then(choice => {
+        if(choice.outcome === 'accepted'){
+            console.log('user accepted the prompt')
+        }
+        defferedPrompt = null;
+    })
+})
