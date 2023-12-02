b7c7f85c1e9e2dc37c7eb99eb0eb780514acde90
Sean Billups <121320002+Backpackinghermit@users.noreply.github.com>
2023-12-02 05:12:35 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent the mini-infobar from appearing on mobile
+  e.preventDefault();
+  // Save the event for later use
+  deferredPrompt = e;
+  // Update UI to show install button
+  btnInstall.style.display = 'block';
+});
+
+btnInstall.addEventListener('click', (e) => {
+  // Show the install prompt
+  deferredPrompt.prompt();
+  // Wait for the user to respond to the prompt
+  deferredPrompt.userChoice.then((choiceResult) => {
+    if (choiceResult.outcome === 'accepted') {
+      console.log('User accepted the install prompt');
+    } else {
+      console.log('User dismissed the install prompt');
+    }
+    deferredPrompt = null;
+  });
+});
+
+window.addEventListener('appinstalled', (evt) => {
+  // App is installed, update your UI accordingly
+});
