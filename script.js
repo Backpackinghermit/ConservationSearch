6d4122a4ee67ed250fe56b417aa515b8b8ec0e75
Backpackinghermit <121320002+Backpackinghermit@users.noreply.github.com>
2023-12-11 21:48:00 +0000

    Update script.js

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
     const installButton = document.getElementById('install-button');
     // Check if the button element is present
     if (installButton) {
-      installButton.style.display = ''; // Set to an empty string to use the default display property
+      installButton.style.display = 'block'; // Use 'block' to explicitly set the display property
       installButton.addEventListener('click', onInstallButtonClick);
     }
   }
